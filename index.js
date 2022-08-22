83e922da7be74d41af39bb2d7717653345944e70
lucastoll <lucastolotto@rocky.ag>
2022-08-22 16:31:23 -0300

    feat: rotas getAllUsers e logon

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 var db = mysql.createConnection({
   host: "localhost",
   user: "root",
+  database: "crud-nodejs",
   password: "",
   port: 3306
 });
@@ -15,15 +18,29 @@ db.connect(function(err) {
   console.log("Connected!");
 });
 
-app.get('/', (req, res) => {
-    const sql = "";
+app.get('/getAllUsers', (req, res) => {
+    const sql = "SELECT * FROM usuarios";
     db.query(sql, function(error, data, fields){
         if (error) throw error;
         res.json({
-            
+            "status": 200,
+            "message": "All users retrieven with success.",
+            "data": data
         })
     })
-    res.send('Hello World!')
+})
+
+app.post('/logon', (req, res) => {
+  const sql = "INSERT INTO usuarios(nome, email) VALUES (?)";
+  const usuario = [req.body.nome, req.body.email]
+
+  db.query(sql, [usuario], function(error, data, fields){
+    if (error) throw error;
+    res.json({
+        "status": 201,
+        "message": "User registered with success."
+    })
+})
 })
   
 app.listen(port, () => {
